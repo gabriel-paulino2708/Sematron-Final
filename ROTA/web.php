a98edc8d447354e829e6b3dd0ab07397c073efa2
Gabriel Paulino <118023793+gabriel-paulino2708@users.noreply.github.com>
2026-01-15 20:52:37 -0300

    Segunda - Só atualizei o arquivo

--- ROTA/web.php
+++ ROTA/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('/htmlDaSematron.inicio');
-});
+Route::get('/', fn () => view('htmlDaSematron.inicio'))->name('home');
+
+Route::get('/inicio', fn () => view('htmlDaSematron.inicio'))->name('inicio');
+
+Route::get('/inscricoes', fn () => view('htmlDaSematron.inscricoes'))->name('inscricoes');
+
+Route::get('/minicursos', fn () => view('htmlDaSematron.minicursos'))->name('minicursos');
+
+Route::get('/visitas', fn () => view('htmlDaSematron.visitas'))->name('visitas');
+
+Route::get('/login', fn () => view('htmlDaSematron.login'))->name('login');
+
+Route::get('/cadastro', fn () => view('htmlDaSematron.cadastro'))->name('cadastro');
+
+Route::get('/maisSematron', fn () => view('htmlDaSematron.maisSematron'))->name('maisSematron');
+
+Route::get('/contato', fn () => view('htmlDaSematron.contato'))->name('contato');
